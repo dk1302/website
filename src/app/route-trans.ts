c7f3d771baff074f8e8cf86636215ec58720bb8d
dk1302 <deank293@gmail.com>
2025-06-15 13:04:51 +0000

    improved style

--- src/app/route-trans.ts
+++ src/app/route-trans.ts
@@ -1,7 +1,7 @@
 import { animate, query, style, transition, trigger } from '@angular/animations';
 
 export const routeTransition = trigger('routeTransition', [
-  transition('* => *', [
+  transition('* => *, void => *', [
     style({ position: 'relative'}),
     query(':enter, :leave', [
       style({ position: 'absolute', top: 0, left: 0, width: '100%' })
@@ -10,10 +10,10 @@ export const routeTransition = trigger('routeTransition', [
       style({ opacity: 0 })
     ], { optional: true }),
     query(':leave', [
-      animate('0.2s', style({ opacity: 0}))
+      animate('0.5s', style({ opacity: 0 }))
     ], { optional: true }),
     query(':enter', [
-      animate('0.2s', style({ opacity: 1 }))
+      animate('0.5s', style({ opacity: 1 }))
     ], { optional: true })
   ])
 ])
